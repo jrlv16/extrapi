008b5d57e5de835a2a88428504c9e577eef78ebe
jrlv16 <jl062705@sfr.fr>
2020-01-31 16:23:36 +0100

    db in settings

--- app/settings.py
+++ app/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER':os.environ.get('DB_USER'),
