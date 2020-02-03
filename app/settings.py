601924616aee88457456b5e99d046556a02c3f32
jrlv16 <jl062705@sfr.fr>
2020-02-03 18:12:47 +0100

    settings

--- app/settings.py
+++ app/settings.py
@@ -80,6 +80,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 
 if 'DATABASE_URL' in os.environ:
+    
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, 
                                                   ssl_require=True)
 else:    
